727fd07aae18fb5e5c21e36e2d881207f62018de
Yeno <43321063+YungYeno@users.noreply.github.com>
2022-01-13 15:52:42 +0100

    Added IPtables function

--- install.py
+++ install.py
@@ -24,7 +24,7 @@ def intro():
 # Install the needed requirements
 def requirements():
     print(f"{GREEN}\nInstalling the project requirements...{WHITE}")
-    command = "sudo apt install git python3-flask python3-flask-login python3-flask-sqlalchemy"
+    command = "sudo apt install git python3-flask python3-flask-login python3-flask-sqlalchemy iptables-persistent"
     try:
         result = os.system(command)
         print(result)
@@ -72,6 +72,25 @@ def permissions():
         print(f"{RED}\nSomething went wrong setting the permissions : {error}")
 
 
+# Add the proper firewall rules for the project
+def iptables():
+    print(f"{GREEN}\nAdding the right firewall configuration...{WHITE}")
+    commands = ["sudo iptables -t NAT -A PREROUTING -i wlan0 -p tcp --dport 80 -j DNAT --to-destination 192.168.4.1:8080",
+                "sudo iptables -t nat -A OUTPUT -d 127.0.0.1 -j DNAT --to-destination 192.168.4.1:8080",
+                "sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 80 -j DNAT --to-destination 192.168.4.1:8080",
+                "sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 443 -j DNAT --to-destination 192.168.4.1:8080",
+                "sudo iptables -t nat -A PREROUTING -p tcp -s 1.1.1.2 --dport 80 -j DNAT --to-destination 192.168.4.1:8080",
+                "sudo iptables -t nat -A PREROUTING -p tcp -m tcp --tcp-flags FIN,SYN FIN,SYN -j DROP",
+                "sudo iptables -t nat -A PREROUTING -p tcp -m tcp --tcp-flags SYN,RST SYN,RST -j DROP"
+                "sudo iptables-save"]
+    try:
+        for command in commands:
+            result = os.system(command)
+            print(result)
+    except OSError as error:
+        print(f"{RED}\nSomething went wrong with adding the firewall rules : {error}")
+
+
 # Let user know that script is completed.
 def outro():
     print(f"{GREEN}\nScript completed! {WHITE}Restart apache2 with {MAGENTA}'systemctl restart apache2'")
@@ -79,8 +98,9 @@ def outro():
 
 if __name__ == '__main__':
     intro()
-    # requirements()
-    # get_project()
-    # moving_files()
-    # permissions()
-    # outro()
+    requirements()
+    get_project()
+    moving_files()
+    permissions()
+    outro()
+    iptables()
